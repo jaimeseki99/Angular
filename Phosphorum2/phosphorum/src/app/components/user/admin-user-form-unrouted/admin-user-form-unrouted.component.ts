6c4bb33567cff172fc69278b33e0b7038aa4a3f0
Jaime Serrano <126757967+jaimeseki99@users.noreply.github.com>
2023-10-23 21:27:08 +0200

    Phosphorum con Borrado de Usuarios

---
+++ Phosphorum2/phosphorum/src/app/components/user/admin-user-form-unrouted/admin-user-form-unrouted.component.ts
@@ -0,0 +1,74 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { IUser, formOperation } from 'src/app/model/model.interfaces';
+
+@Component({
+  selector: 'app-admin-user-form-unrouted',
+  templateUrl: './admin-user-form-unrouted.component.html',
+  styleUrls: ['./admin-user-form-unrouted.component.css']
+})
+export class AdminUserFormUnroutedComponent implements OnInit {
+
+  @Input() id: number = 1;
+  @Input() operation: formOperation = 'NEW'; //new or edit
+
+  userForm!: FormGroup;
+  oUser: IUser = {} as IUser;
+  status: HttpErrorResponse | null = null;
+
+  constructor(private fb: FormBuilder, private oHttpClient: HttpClient) {
+    //this.userForm = null;
+    this.userForm = this.fb.group({
+      id: [''],
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      lastname: [''],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', Validators.required],
+      role: ['']
+    });
+  }
+
+  ngOnInit() {
+    if (this.operation == 'EDIT') {
+      this.oHttpClient.get<IUser>("http://localhost:8083/user/" + this.id).subscribe({
+        next: (data: IUser) => {
+          this.oUser = data;
+          this.userForm = this.fb.group({
+            id: [this.oUser.id],
+            name: [this.oUser.name, Validators.required],
+            surname: [this.oUser.surname, Validators.required],
+            lastname: [this.oUser.lastname],
+            email: [this.oUser.email, [Validators.required, Validators.email]],
+            username: [this.oUser.username, Validators.required],
+            role: [this.oUser.role]
+          });
+        },
+        error: (error: HttpErrorResponse) => {
+          this.status = error;
+        }
+      })
+
+    } else {
+      this.userForm = this.fb.group({
+        id: [''],
+        name: ['', Validators.required],
+        surname: ['', Validators.required],
+        lastname: [''],
+        email: ['', [Validators.required, Validators.email]],
+        username: ['', Validators.required],
+        role: ['']
+      });
+    }
+
+  }
+
+  onSubmit() {
+    //if (this.userForm.valid) {
+    // Aquí puedes enviar los datos del usuario al servidor o realizar otras acciones necesarias.
+    //  console.log(this.userForm.value);
+    //}
+  }
+
+}
---
+++ Phosphorum2/phosphorum/src/app/components/user/admin-user-plist-unrouted/admin-user-plist-unrouted.component.ts
@@ -0,0 +1,108 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { PaginatorState } from 'primeng/paginator';
+import { IUser, IUserPage } from 'src/app/model/model.interfaces';
+import { AdminUserDetailUnroutedComponent } from '../admin-user-detail-unrouted/admin-user-detail-unrouted.component';
+
+
+@Component({
+  selector: 'app-admin-user-plist-unrouted',
+  templateUrl: './admin-user-plist-unrouted.component.html',
+  styleUrls: ['./admin-user-plist-unrouted.component.css']
+})
+
+export class AdminUserPlistUnroutedComponent implements OnInit {
+
+  oPage: any = [];
+  orderField: string = "id";
+  orderDirection: string = "asc";
+  oPaginatorState: PaginatorState = { first: 0, rows: 10, page: 0, pageCount: 0 };
+  status: HttpErrorResponse | null = null;
+
+  displayDialog: boolean = false;
+  displayDeleteDialog: boolean = false;
+  userToDelete: IUser | null = null;
+
+  constructor(
+    private oHttpClient: HttpClient,
+    public dialogService: DialogService,
+    public messageService: MessageService
+  ) { }
+
+  ngOnInit() {
+    this.getPage();
+  }
+
+  getPage(): void {
+    this.oHttpClient.get<IUserPage>("http://localhost:8083/user" + "?size=" + this.oPaginatorState.rows + "&page=" + this.oPaginatorState.page + "&sort=" + this.orderField + "," + this.orderDirection).subscribe({
+      next: (data: IUserPage) => {
+        this.oPage = data;
+        this.oPaginatorState.pageCount = data.totalPages;
+        console.log(this.oPaginatorState);
+      },
+      error: (error: HttpErrorResponse) => {
+        this.oPage.error = error;
+        this.status = error;
+      }
+    })
+  }
+
+  onPageChang(event: PaginatorState) {
+    this.oPaginatorState.rows = event.rows;
+    this.oPaginatorState.page = event.page;
+    this.getPage();
+  }
+
+  doOrder(fieldorder: string) {
+    this.orderField = fieldorder;
+    if (this.orderDirection == "asc") {
+      this.orderDirection = "desc";
+    } else {
+      this.orderDirection = "asc";
+    }
+    this.getPage();
+  }
+
+  ref: DynamicDialogRef | undefined;
+
+  goToView(u: IUser) {
+    this.ref = this.dialogService.open(AdminUserDetailUnroutedComponent, {
+      data: {
+        id: u.id
+      },
+      header: 'View of user',
+      width: '70%',
+      contentStyle: { overflow: 'auto' },
+      baseZIndex: 10000,
+      maximizable: false
+    });
+  }
+ 
+  confirmDelete(user: IUser) {
+    this.userToDelete = user;
+    this.displayDialog = true;
+  }
+
+  cancelDelete() {
+    this.displayDialog = false;
+    this.userToDelete = null;
+  }
+
+  deleteUser() {
+    if (this.userToDelete) {
+      this.oHttpClient.delete(`http://localhost:8083/user/${this.userToDelete.id}`).subscribe(() => {
+        this.displayDialog = false;
+        this.userToDelete = null;
+        this.getPage(); // Recargar la lista de usuarios.
+      });
+    }
+  }
+
+
+ 
+
+ 
+
+}
